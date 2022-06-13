94e9ed24da9f573e69fa65527ff739d71169320b
Lux <92374048+lucas38460@users.noreply.github.com>
2022-06-13 11:11:06 +0200

    fin exo

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const cards = document.querySelectorAll('.cards');
-const check = document.querySelectorAll('.red');
+const cards = document.querySelectorAll(".cards");
 let count = 0;
 
 for (let i = 0; i < cards.length; i++) {
   const element = cards[i];
-  element.addEventListener('click',()=>{
+  element.addEventListener("click", () => {
     cards[i].classList.toggle("red");
-    number(check);
+    const check = document.querySelectorAll(".red");
+    if (element.classList.contains("red")) {
+      count++;
+      const nbrcheck = (document.getElementById(
+        "nbrcheck"
+      ).innerHTML = `Vous avez sélectionné ${count} cartes`);
+    } else {
+      count--;
+      const nbrcheck = (document.getElementById(
+        "nbrcheck"
+      ).innerHTML = `Vous avez sélectionné ${count} cartes`);
+    }
   });
-}
-
-function number(check) {
-    count++;
-  const nbrcheck = document.getElementById('nbrcheck').innerHTML = `Vous avez sélectionné ${count} cartes`;
 }
